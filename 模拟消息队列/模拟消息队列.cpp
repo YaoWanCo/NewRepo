643043816fd5a18e2d02b79c077376408300bcf9
YaoWanCo <1807005258@qq.com>
2025-05-05 20:56:36 +0800

    更新

--- 模拟消息队列/模拟消息队列.cpp
+++ 模拟消息队列/模拟消息队列.cpp
@@ -2,19 +2,91 @@
 //
 
 #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string a, b;
+    while (getline(cin, a)) {
+        getline(cin, b);
+        map<int, int> event;
+        vector<pair<int, int>> people;
+        int x = 0 , y = 0;
+        for (int k = 0; k < a.size(); k++) {
+            char i = a[k];
+            if (i == ' ' || k == a.size() - 1) {
+                if (y == 0) {
+                    y = x;
+                    x = 0;
+                }
+                else if (k == a.size() - 1) {
+                    x = x * 10 + (i - '0');
+                    event[y] = x;
+                    x = 0; y = 0;
+                }
+                else {
+                    event[y] = x;
+                    x = 0; y = 0;
+                }
+            }
+            else x = x * 10 + (i - '0');
+        }
+        for (int k = 0; k < b.size(); k++) {
+            char i = b[k];
+            if (i == ' ' || k == b.size() - 1) {
+                if (y == 0) {
+                    y = x;
+                    x = 0;
+                }
+                else if (k == b.size() - 1) {
+                    x = x * 10 + (i - '0');
+                    people.emplace_back(make_pair(y, x));
+                    x = 0; y = 0;
+                }
+                else {
+                    people.emplace_back(make_pair(y, x));
+                    x = 0; y = 0;
+                }
+            }
+            else x = x * 10 + (i - '0');
+        }
+        vector<vector<int>> ans(people.size());
+        for (auto i : event) {
+            for (int j = people.size() - 1; j >= 0; j--) {
+                if (i.first >= people[j].first && i.first < people[j].second) {
+                    ans[j].emplace_back(i.second);
+                    break;
+                }
+            }
+        }
+        for (auto i : ans) {
+            for (auto j : i) {
+                cout << j << ' ';
+            }
+            cout << endl;
+        }
+    }
+
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+/*
+输入
+2 22 1 11 4 44 5 55 3 33
+1 7 2 3
+输出
+11 33 44 55
+22
+输入
+5 64 11 64 9 97
+9 11 4 9
+输出
+97
+64
+说明
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+消息11在1时刻到达，此时只有第一个消费者订阅，消息发送给它，
+消息22在2时刻到达，此时两人消费者都订阅了，消息发送给优先级最高的第二个消费者 :
+消息33在时刻3到达，此时只有第一个消费者订阅，消息发送给它;
+余下的消息按规则也是发送给第一个消费者
+*/ 
